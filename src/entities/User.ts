3cb02fbf14dcc048a460d25d4ae10df47fcd6f1b
YoussefPasha <jojoyossefbasha010@gmail.com>
2021-08-24 06:27:54 +0200

    add register route trim middleware

--- src/entities/User.ts
+++ src/entities/User.ts
@@ -1,18 +1,57 @@
-import {Entity, PrimaryGeneratedColumn, Column} from "typeorm";
+import { IsEmail, Length } from "class-validator";
+import {
+  Entity,
+  PrimaryGeneratedColumn,
+  Column,
+  BaseEntity,
+  Index,
+  CreateDateColumn,
+  UpdateDateColumn,
+  BeforeInsert,
+} from "typeorm";
+import bcrypt from "bcrypt";
+import { classToPlain, Exclude } from "class-transformer";
 
-@Entity('users')
-export class User {
+@Entity("users")
+export class User extends BaseEntity {
+  constructor(user: Partial<User>) {
+    super();
+    Object.assign(this, user);
+  }
 
-    @PrimaryGeneratedColumn()
-    id: number;
+  @Exclude()
+  @PrimaryGeneratedColumn()
+  id: number;
 
-    @Column()
-    firstName: string;
+  @Index()
+  @IsEmail()
+  @Column({ unique: true })
+  email: string;
 
-    @Column()
-    lastName: string;
+  @Index()
+  @Length(3, 255, {
+    message: "Username must be at least 3 characters long",
+  })
+  @Column({ unique: true })
+  username: string;
 
-    @Column()
-    age: number;
+  @Exclude()
+  @Length(6)
+  @Column()
+  password: string;
 
+  @CreateDateColumn()
+  createAt: Date;
+
+  @UpdateDateColumn()
+  updateAt: Date;
+
+  @BeforeInsert()
+  async hashPassword() {
+    this.password = await bcrypt.hash(this.password, 6);
+  }
+
+  toJson() {
+    return classToPlain(this);
+  }
 }
--- src/server.ts
+++ src/server.ts
@@ -2,12 +2,17 @@ import "reflect-metadata";
 import { createConnection } from "typeorm";
 import express from "express";
 import morgan from "morgan";
+import authRoutes from "./routes/auth";
+import trim from "./middleware/trim";
+
 const app = express();
 
 app.use(express.json());
+app.use(trim);
 app.use(morgan("dev"));
 
 app.get("/", (req, res) => res.send("Hello World"));
+app.use("/api/auth", authRoutes);
 
 app.listen(5000, async () => {
   console.log("running...");
